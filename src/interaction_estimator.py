cb383fea924bf5993ed2fe44363d5186c49f5eeb
minghao0408 <minghao.zhao@studio.unibo.it>
2024-08-12 17:25:39 +0200

    Create interaction_estimator.py

---
+++ src/interaction_estimator.py
@@ -0,0 +1,6 @@
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+def estimate_interaction(cell_centers: np.ndarray, cell_types: np.ndarray) -> np.ndarray:
+    # Implementation as before
+    pass
