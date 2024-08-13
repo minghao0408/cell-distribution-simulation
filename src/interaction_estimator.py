26a96c994c385d2081329b8d4c3dcadba7cc0507
minghao0408 <minghao.zhao@studio.unibo.it>
2024-08-13 19:20:57 +0200

    Delete src directory

--- src/interaction_estimator.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from scipy.spatial.distance import pdist, squareform
-
-def estimate_interaction(cell_centers: np.ndarray, cell_types: np.ndarray) -> np.ndarray:
-    # Implementation as before
-    pass
