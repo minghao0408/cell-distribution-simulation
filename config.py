5cf2773eea94d187a783e8de78d1343fe11268a6
minghao0408 <minghao.zhao@studio.unibo.it>
2024-09-11 14:52:26 +0200

    Update config.py
    Update the  dictionary in  with the new values provided in the artifact.

--- config.py
+++ config.py
@@ -1,17 +1,23 @@
 # config.py
 
 import numpy as np
-from scipy.stats import norm
+from scipy import stats
 
 CONFIG = {
     'mask_size': 100,
-    'forbidden_region': ((40, 60), (40, 60)),
-    'cell_counts': [50, 50],
-    'x_distributions': [norm(loc=-1, scale=0.5), norm(loc=1, scale=0.5)],
-    'y_distributions': [norm(loc=-1, scale=0.5), norm(loc=1, scale=0.5)],
-    'interaction_matrix': np.array([[1.0, 0.3],
-                                    [0.3, 1.0]]),
-    'min_distance': 0.2,
+    'forbidden_region': ((45, 55), (45, 55)),  # Adjust forbidden region
+    'cell_counts': [200, 200],
+    'x_distributions': [
+        stats.truncnorm(a=-2, b=2, loc=0.5, scale=0.2),
+        stats.truncnorm(a=-2, b=2, loc=0.5, scale=0.2)
+    ],
+    'y_distributions': [
+        stats.truncnorm(a=-2, b=2, loc=0.5, scale=0.2),
+        stats.truncnorm(a=-2, b=2, loc=0.5, scale=0.2)
+    ],
+    'interaction_matrix': np.array([[1.0, 0.5],  # Adjust interaction strength
+                                    [0.5, 1.0]]),
+    'min_distance': 0.02,  # Adjust minimum distance
     'plot_size': (10, 10),
-    'plot_extent': [-3, 3, -3, 3],
+    'plot_extent': [0, 1, 0, 1]
 }
