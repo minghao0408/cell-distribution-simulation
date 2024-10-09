07d3ddb5e04eff557c95b5ae3cddeecf6ec6d499
minghao0408 <minghao.zhao@studio.unibo.it>
2024-10-09 15:29:35 +0200

    Update test_simulation.py

--- test_simulation.py
+++ test_simulation.py
@@ -1,41 +1,51 @@
-# test_simulation.py
-
 import unittest
 import numpy as np
-from simulation import create_binary_mask, generate_cell_centers, estimate_interaction
-from config import CONFIG
+from scipy import stats
+from simulation import create_binary_mask, simulate_cell_distribution, generate_cell_centers
 
 class TestSimulation(unittest.TestCase):
     def test_create_binary_mask(self):
-        mask = create_binary_mask(CONFIG['mask_size'], CONFIG['forbidden_region'])
-        self.assertEqual(mask.shape, (CONFIG['mask_size'], CONFIG['mask_size']))
-        self.assertFalse(np.all(mask[40:60, 40:60]))
-        self.assertTrue(np.all(mask[0:40, 0:40]))
+        mask = create_binary_mask(100, ((45, 55), (45, 55)))
+        self.assertEqual(mask.shape, (100, 100))
+        self.assertFalse(np.all(mask[45:55, 45:55]))
+        self.assertTrue(np.all(mask[0:45, 0:45]))
+
+    def test_simulate_cell_distribution(self):
+        allowed_region = np.ones((100, 100), dtype=bool)
+        x_dist = stats.uniform(loc=0, scale=1)
+        y_dist = stats.uniform(loc=0, scale=1)
+        
+        result = simulate_cell_distribution(
+            x_distribution=x_dist,
+            y_distribution=y_dist,
+            cell_count=50,
+            allowed_region=allowed_region,
+            cell_interaction_radius=0.05,
+            cell_radius=0.01,
+            show_plots=False
+        )
+        
+        self.assertEqual(len(result), 6)
+        self.assertGreater(result[0] + result[1] + result[2], 0)  # At least some pixels should be covered
 
     def test_generate_cell_centers(self):
-        binary_mask = create_binary_mask(CONFIG['mask_size'], CONFIG['forbidden_region'])
-        cell_data = generate_cell_centers(
-            CONFIG['cell_counts'],
-            CONFIG['x_distributions'],
-            CONFIG['y_distributions'],
-            binary_mask,
-            CONFIG['interaction_matrix'],
-            CONFIG['min_distance']
-        )
-        self.assertIsInstance(cell_data, np.ndarray)
-        if cell_data.size > 0:
-            self.assertEqual(cell_data.shape[1], 3)  # x, y, cell_type
-
-    def test_estimate_interaction(self):
-        # Generate some mock data
-        cell_centers = np.random.rand(100, 2)  # Range from 0 to 1
-        cell_types = np.random.randint(0, 2, 100)
+        binary_mask = np.ones((100, 100), dtype=bool)
+        x_dist = stats.uniform(loc=0, scale=1)
+        y_dist = stats.uniform(loc=0, scale=1)
+        interaction_matrix = np.array([[1.0, 0.5], [0.5, 1.0]])
         
-        estimated_interaction = estimate_interaction(cell_centers, cell_types, CONFIG['interaction_matrix'])
-        self.assertIsInstance(estimated_interaction, np.ndarray)
-        if estimated_interaction.size > 0:
-            self.assertEqual(estimated_interaction.shape, (2, 2))
-            self.assertTrue(np.allclose(estimated_interaction, estimated_interaction.T))  # Should be symmetric
+        cell_data = generate_cell_centers(
+            N=[50, 50],
+            x_distribution=[x_dist, x_dist],
+            y_distribution=[y_dist, y_dist],
+            binary_mask=binary_mask,
+            interaction_matrix=interaction_matrix,
+            min_distance=0.01
+        )
+        
+        self.assertIsInstance(cell_data, np.ndarray)
+        self.assertEqual(cell_data.shape[1], 3)  # x, y, cell_type
+        self.assertEqual(len(np.unique(cell_data[:, 2])), 2)  # Two cell types
 
 if __name__ == '__main__':
     unittest.main()
